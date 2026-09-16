2fac09b5083ecab12595c722acf20309ed2dc21d
vmonke67 <vmonke@student.tgm.ac.at>
2026-09-16 23:28:08 +0200

    GewinnModel ready

--- src/GewinnModel.java
+++ src/GewinnModel.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class GewinnModel {
     private int gesamtPunkte;
     private int spielerZahl;
@@ -11,6 +13,10 @@ public class GewinnModel {
         rundenErgebnis = 0;
     }
 
+    public void setSpielerZahl(int spielerZahl) {
+        this.spielerZahl = spielerZahl;
+    }
+
     public int getGesamtPunkte() {
         return gesamtPunkte;
     }
@@ -21,5 +27,31 @@ public class GewinnModel {
 
     public int getRundenErgebnis() {
         return rundenErgebnis;
+    }
+
+    public void ComputerZahl() {
+        Random rand = new Random();
+        computerZahl = rand.nextInt(9) + 1;
+    }
+
+    public void berechneRunde() {
+        int punktenBekommen;
+        if (spielerZahl == computerZahl) {
+            punktenBekommen = 20;
+        } else  if (spielerZahl + 1 == computerZahl || spielerZahl -1 == computerZahl) {
+            punktenBekommen = 5;
+        } else {
+            punktenBekommen = -10;
+        }
+        rundenErgebnis = punktenBekommen;
+        gesamtPunkte = rundenErgebnis;
+    }
+
+    public boolean hatGewonnen() {
+        return gesamtPunkte >= 100;
+    }
+
+    public boolean hatVerloren() {
+        return gesamtPunkte <= 0;
     }
 }
