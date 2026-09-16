faf4f87672f0b9aa47ebcf40d5e8bc044d574c48
vmonke67 <vmonke@student.tgm.ac.at>
2026-09-16 23:21:20 +0200

    fields and constructor ready

---
+++ src/GewinnModel.java
@@ -0,0 +1,13 @@
+public class GewinnModel {
+    private int gesamtPunkte;
+    private int spielerZahl;
+    private int computerZahl;
+    private int rundenErgebnis;
+
+    public GewinnModel() {
+        gesamtPunkte = 30;
+        spielerZahl = 0;
+        computerZahl = 0;
+        rundenErgebnis = 0;
+    }
+}
